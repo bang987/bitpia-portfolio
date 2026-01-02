e41d95d5a6420c59e2b890df44782fca9e68b42a
bang987 <65885794+bang987@users.noreply.github.com>
2026-01-02 21:48:19 +0900

    Update portfolio title and description text

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,10 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Bitpia.dev
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`"복잡한 서류 업무부터 메일 대응까지, 당신의 비즈니스를 실행하는 AI 직원을 만들어 드립니다."`}
       </p>
       <div className="my-8">
         <BlogPosts />
